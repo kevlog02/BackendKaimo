b94a7ad65c136ad438d13e471e68e810cacb0331
kevlog02 <kevinandresruro10@gmail.com>
2025-11-29 00:54:53 -0700

    cambio de nombre en la database

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,7 @@ DATABASE_USER = os.environ.get("MYSQLUSER", "root")
 DATABASE_PASSWORD = os.environ.get("MYSQLPASSWORD", "chAVuwbfFRQeTppuvhpbMhkVGfkBqvqZ")
 DATABASE_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
 DATABASE_PORT = os.environ.get("MYSQLPORT", "3306")
-DATABASE_NAME = os.environ.get("MYSQLDATABASE", "railway")
+DATABASE_NAME = os.environ.get("MYSQLDATABASE", "Kaimo")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
