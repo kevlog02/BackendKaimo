3a1f55719d8f155a55f4ae9fa52d84a435f4cff0
kevlog02 <kevinandresruro10@gmail.com>
2025-11-28 23:34:38 -0700

    adaptando database a railway

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,17 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-# Cargar variables de entorno
-load_dotenv()
-
-# Construir URL desde variables de entorno
-DATABASE_USER = os.getenv("DATABASE_USER", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "Kevin0224")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "kaimo_bd")
+DATABASE_USER = os.environ["MYSQLUSER"]
+DATABASE_PASSWORD = os.environ["MYSQLPASSWORD"]
+DATABASE_HOST = os.environ["MYSQLHOST"]
+DATABASE_PORT = os.environ.get("MYSQLPORT", "3306")
+DATABASE_NAME = os.environ["MYSQLDATABASE"]
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
@@ -23,7 +18,6 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
