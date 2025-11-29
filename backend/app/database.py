7aaacc234dc834649f197e32720abd8246c107c7
kevlog02 <kevinandresruro10@gmail.com>
2025-11-28 23:37:56 -0700

    ahora si database actualizada para deploy

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
 DATABASE_USER = os.environ["MYSQLUSER"]
 DATABASE_PASSWORD = os.environ["MYSQLPASSWORD"]
@@ -11,12 +11,7 @@ DATABASE_NAME = os.environ["MYSQLDATABASE"]
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=3600
-)
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
