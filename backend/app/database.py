a580c0b0319838fbb9e5b9336b1183bcaaa34355
kevlog02 <kevinandresruro10@gmail.com>
2025-12-03 16:31:26 -0700

    feat: Add SQLAlchemy database connection setup and `get_db` dependency.

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,11 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Construir URL desde variables de entorno
-DATABASE_USER = os.getenv("DATABASE_USER", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "Kevin0224")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "kaimo_bd")
+DATABASE_USER = os.environ.get("MYSQLUSER", "root")
+DATABASE_PASSWORD = os.environ.get("MYSQLPASSWORD", "chAVuwbfFRQeTppuvhpbMhkVGfkBqvqZ")
+DATABASE_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
+DATABASE_PORT = os.environ.get("MYSQLPORT", "29893")
+DATABASE_NAME = os.environ.get("MYSQLDATABASE", "railway")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
