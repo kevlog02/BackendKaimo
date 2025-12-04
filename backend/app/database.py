b895e87a7becdb556eb89d25572e7c630b41a6d3
kevlog02 <kevinandresruro10@gmail.com>
2025-12-03 21:01:22 -0700

    cambio del database para el deploy nuevamente

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,19 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_USER = os.getenv("MYSQLUSER")
-DATABASE_PASSWORD = os.getenv("MYSQLPASSWORD")
-DATABASE_HOST = os.getenv("MYSQLHOST")
-DATABASE_PORT = int(os.getenv("MYSQLPORT"))
-DATABASE_NAME = os.getenv("MYSQLDATABASE")
-
+DATABASE_USER = os.getenv("MYSQLUSER") or os.getenv("MYSQL_USER")
+DATABASE_PASSWORD = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_PASSWORD")
+DATABASE_HOST = os.getenv("MYSQLHOST") or os.getenv("MYSQL_HOST")
+DATABASE_PORT = int(os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT") or 3306)
+DATABASE_NAME = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE")
 SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}"
-    f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 )
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 Base = declarative_base()
