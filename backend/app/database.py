f1cf1df5c926801b488f2d2c5dc88d1ce052e7b3
kevlog02 <kevinandresruro10@gmail.com>
2025-12-03 21:12:32 -0700

    calando nuevo database al que estaba antes

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,21 +1,23 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+DATABASE_USER = os.environ.get("MYSQLUSER", "root")
+DATABASE_PASSWORD = os.environ.get("MYSQLPASSWORD", "chAVuwbfFRQeTppuvhpbMhkVGfkBqvqZ")
+DATABASE_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
+DATABASE_PORT = os.environ.get("MYSQLPORT", "3306")
+DATABASE_NAME = os.environ.get("MYSQLDATABASE", "railway")
 
-DATABASE_USER = os.getenv("MYSQLUSER") or os.getenv("MYSQL_USER")
-DATABASE_PASSWORD = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_PASSWORD")
-DATABASE_HOST = os.getenv("MYSQLHOST") or os.getenv("MYSQL_HOST")
-DATABASE_PORT = int(os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT") or 3306)
-DATABASE_NAME = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE")
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
