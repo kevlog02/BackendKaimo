c5f1c37c8d6d65d2311a93ea27a7105873e5a69f
kevlog02 <kevinandresruro10@gmail.com>
2025-12-04 00:46:50 -0700

    cambio en allow origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 #CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=get_cors_origins(),
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
