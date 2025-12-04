454d1acd5fe4f5f21a7cbcf927394df539dad1fe
kevlog02 <kevinandresruro10@gmail.com>
2025-12-04 00:16:00 -0700

    error de cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 #CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url],
+    allow_origins=get_cors_origins(),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
